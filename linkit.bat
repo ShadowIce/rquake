41d3ea3c8f35a1baffda043827e42e426db8c256
Maurice Gilden <MauriceG@gmx.net>
2016-01-22 23:53:27 +0100

    Debug build should now open a console window while release build should not.

--- linkit.bat
+++ linkit.bat
@@ -1,1 +1,20 @@
+@echo off
+setlocal enableextensions enabledelayedexpansion
+
+rem Only works if "release" is not part of any path/parameter in debug builds.
+
+for %%i in (%*) do (
+    set t1=%%i
+	set t2=!t1:release=foundit!
+	if not !t1!==!t2! goto :release
+)
+endlocal
+
+
+:debug
+gcc -static-libgcc res/rquake.res %*
+goto :eof
+
+:release
 gcc -mwindows -static-libgcc res/rquake.res %*
+goto :eof
