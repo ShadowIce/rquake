b857092c25798f2e70602e40dcd99ce0ff8e3765
Maurice Gilden <MauriceG@gmx.net>
2017-01-05 15:27:43 +0100

    Improved build.rs for non-Windows targets.

--- build.rs
+++ build.rs
@@ -1,16 +1,20 @@
 #[cfg(windows)]
 extern crate winres;
 
+#[cfg(windows)]
 fn main() {
-    if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("res/quake.ico")
-            .set("InternalName", "RQUAKE.EXE")
-            // manually set version 1.0.0.0
-            .set_version_info(winres::VersionInfo::PRODUCTVERSION, 0x0001000000000000);
-        match res.compile() {
-            Ok(_) => {},
-            Err(_) => panic!("Resource could not be compiled"),
-        };
-    }
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("res/quake.ico")
+        .set("InternalName", "RQUAKE.EXE")
+        // manually set version 1.0.0.0
+        .set_version_info(winres::VersionInfo::PRODUCTVERSION, 0x0001000000000000);
+    match res.compile() {
+        Ok(_) => {},
+        Err(_) => panic!("Resource could not be compiled"),
+    };
+}
+
+#[cfg(not(windows))]
+fn main() {
+    
 }
